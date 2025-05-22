726b8a6ce425df1dc6af6b72d8cce8b6795f73bb
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2025-05-22 09:42:32 +0300

    with Update

--- DemoApp2/Controllers/EmployeesController.cs
+++ DemoApp2/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using DemoApp2.Data;
+using DemoApp2.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApp2.Controllers
@@ -20,6 +21,32 @@ namespace DemoApp2.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee) 
+        {
+            employee.IsActive = true;
+            // Save Data
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id) 
+        {
+            if (id==null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var empData = db.Employees.Find(id);
+            if (empData != null) 
+            {
+                return View(empData);
+
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
